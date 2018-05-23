4efb431dbc67209325178ba6d760e1601e0de41b
DELL <DELL@DELL-PC>
2018-05-23 18:59:45 +0530

    Commit Java Demo

---
+++ JavaDemo/src/com/testersconnect/code/EvenOdd.java
@@ -0,0 +1,14 @@
+package com.testersconnect.code;
+
+public class EvenOdd {
+	public void isEven() {
+		int n =30;
+		if (n%2==0)
+			System.out.println("Number is Even");
+		else
+			System.out.println("Number is Odd");
+						
+	}
+	
+
+}
---
+++ JavaDemo/src/com/testersconnect/code/TestClass.java
@@ -0,0 +1,13 @@
+package com.testersconnect.code;
+
+public class TestClass {
+	
+	public static void main(String[]args) { 
+		
+		EvenOdd eo = new EvenOdd();
+		eo.isEven();
+		
+		//System.out.println("Hello World");
+	}
+
+}
